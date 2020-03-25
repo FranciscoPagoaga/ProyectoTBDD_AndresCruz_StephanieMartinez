a7d19b540edb7d173c554debfee4af9565a2509d
Stephanie Martínez <stephanie_martinezh@hotmail.com>
2020-03-25 01:48:17 -0600

    Update

--- ProyectoTBDD_AndresCruz_StephanieMartinez/src/proyectotbdd_andrescruz_stephaniemartinez/BaseDeDatos.java
+++ ProyectoTBDD_AndresCruz_StephanieMartinez/src/proyectotbdd_andrescruz_stephaniemartinez/BaseDeDatos.java
@@ -155,7 +155,7 @@ public class BaseDeDatos {
         int resultado = 0;
         try {
             // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
-            procedimiento = coneccion.prepareCall(" CALL PAIVenta(?,?,?,?,?,?,?,?,?) ");
+            procedimiento = coneccion.prepareCall(" CALL PAIVenta(?,?,?,?,?) ");
             //se cargan los parametros de entrada
             procedimiento.setString("rtnConcesionarioVenta", rtnConcesionarioVenta);
             procedimiento.setString("rtnClienteVenta", rtnClienteVenta);
@@ -292,4 +292,151 @@ public class BaseDeDatos {
         }
         return resultado;
     }//Fin del método de delete en la tabla Venta
+
+    public int procedimientoActualizarCliente(String rtnCliente, String nombreCliente, int telefonoCliente, String direccionCliente, int esCompCliente, String idCliente, String sexoCliente, float ingresoAnualCliente, int tipoCompaniaCliente) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUCliente(?,?,?,?,?,?,?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("rtnCliente", rtnCliente);
+            procedimiento.setString("nombreCliente", nombreCliente);
+            procedimiento.setInt("telefonoCliente", telefonoCliente);
+            procedimiento.setString("direccionCliente", direccionCliente);
+            procedimiento.setInt("esCompCliente", esCompCliente);
+            procedimiento.setString("idCliente", idCliente);
+            procedimiento.setString("sexoCliente", sexoCliente);
+            procedimiento.setFloat("ingresoAnualCliente", ingresoAnualCliente);
+            procedimiento.setInt("tipoCompaniaCliente", tipoCompaniaCliente);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Cliente
+
+    public int procedimientoActualizarCompania(String idCompania, String marcaCompania, String nombreCompania) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUCompania(?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("idCompania", idCompania);
+            procedimiento.setString("marcaCompania", marcaCompania);
+            procedimiento.setString("nombreCompania", nombreCompania);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Compania
+    
+    public int procedimientoActualizarConcesionario(String rtnConcesionario, String nombreConcesionario, String ubicacionConcesionario) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUConcesionario(?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("rtnConcesionario", rtnConcesionario);
+            procedimiento.setString("nombreConcesionario", nombreConcesionario);
+            procedimiento.setString("ubicacionConcesionario", ubicacionConcesionario);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Concesionario
+    
+    public int procedimientoActualizarPlanta(String idPlanta, String nombrePlanta, String tipoPlanta) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUPlanta(?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("idPlanta", idPlanta);
+            procedimiento.setString("nombrePlanta", nombrePlanta);
+            procedimiento.setString("tipoPlanta", tipoPlanta);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Planta
+    
+    public int procedimientoActualizarProveedor(String idProveedor, String nombreProveedor, String piezaProveedor) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUProveedor(?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("idProveedor", idProveedor);
+            procedimiento.setString("nombreProveedor", nombreProveedor);
+            procedimiento.setString("piezaProveedor", piezaProveedor);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Proveedor
+    
+    public int procedimientoActualizarVehiculo(String vinVehiculo, String modeloVehiculo, String tipoCarroceria, String tipoMotor, String colorVehiculo,
+            String transmisionVehiculo, Date fechaEnsamblaje, float precioVehiculo) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAIVehiculo(?,?,?,?,?,?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("vinVehiculo", vinVehiculo);
+            procedimiento.setString("modeloVehiculo", modeloVehiculo);
+            procedimiento.setString("tipoCarroceria", tipoCarroceria);
+            procedimiento.setString("tipoMotor", tipoMotor);
+            procedimiento.setString("colorVehiculo", colorVehiculo);
+            procedimiento.setString("transmisionVehiculo", transmisionVehiculo);
+            procedimiento.setDate("fechaEnsamblaje", fechaEnsamblaje);
+            procedimiento.setFloat("precioVehiculo", precioVehiculo);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Vehiculo
+    
+    public int procedimientoActualizarVenta(String rtnConcesionarioVenta, String rtnClienteVenta, String vinVenta, String tipoMotor, String colorVehiculo, String transmisionVehiculo, Date fechaVenta, float precioVenta, String idCompaniaVehiculo) {
+        int resultado = 0;
+        try {
+            // se crea instancia a procedimiento, los parametros de entrada y salida se simbolizan con el signo ?
+            procedimiento = coneccion.prepareCall(" CALL PAUVenta(?,?,?,?,?) ");
+            //se cargan los parametros de entrada
+            procedimiento.setString("rtnConcesionarioVenta", rtnConcesionarioVenta);
+            procedimiento.setString("rtnClienteVenta", rtnClienteVenta);
+            procedimiento.setString("vinVenta", vinVenta);
+            procedimiento.setDate("fechaVenta", fechaVenta);
+            procedimiento.setFloat("precioVenta", precioVenta);
+            // Se ejecuta el procedimiento almacenado
+            procedimiento.execute();
+            // devuelve el valor del parametro de salida del procedimiento
+            resultado = procedimiento.getInt("condicion");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }//Fin del método de update en la tabla Venta
 }
